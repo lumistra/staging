94504ad7764ef576157ade902c5cddbeb831893d
Dario Dumlijan <dario.dumlijan1@gmail.com>
2024-06-08 21:44:49 +0200

    fix: Correct email domain

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,7 +2,7 @@ import Facebook from '@/assets/svg/facebook.svg';
 import Instagram from '@/assets/svg/instagram.svg';
 import LinkedIn from '@/assets/svg/linkedin.svg';
 
-export const email = 'info@lumistra.com';
+export const email = 'info@studiolumistra.com';
 export const socials = [
   {
     label: 'Instagram',
