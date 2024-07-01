d7cace7d873524726cb843c721c8c4a7e9112fd0
Dario Dumlijan <dario.dumlijan1@gmail.com>
2024-07-01 17:53:29 +0200

    feat: Footer parallax animation

--- src/components/containers/Footer.tsx
+++ src/components/containers/Footer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { map } from 'lodash';
+import { floor, map } from 'lodash';
 import Icon from '@/assets/svg/icon.svg';
 import Logo from '@/assets/svg/logo.svg';
 import Logotype from '@/assets/svg/logotype.svg';
@@ -11,14 +11,18 @@ import Link from '../elements/Link';
 import LocaleSwitcher from '../elements/LocaleSwitcher';
 import ToTop from '../elements/ToTop';
 
+const parallaxPercentage = 25;
+
 export default function Footer() {
   const { t } = useTranslations();
   const { isTablet, isLaptop } = useScreenSize();
+  const [animationOffset, setAnimationOffset] = useState(parallaxPercentage);
   const [bottomOffset, setBottomOffset] = useState(0);
   const footerRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    if (!footerRef.current) return;
+    const page = document.getElementById('page-wrapper');
+    if (isTablet || !footerRef.current || !page) return;
 
     const handleFooterSizeChange = () => {
       if (!footerRef.current) return;
@@ -27,9 +31,27 @@ export default function Footer() {
       setBottomOffset(requiredOffset < 0 ? 0 : -requiredOffset);
     };
 
+    const handleAnimation = () => {
+      if (!page) return;
+      const pageVisibleOnScreen = page.clientHeight - window.scrollY;
+      const footerVisibleOnScreen = window.innerHeight - pageVisibleOnScreen;
+
+      if (footerVisibleOnScreen < 0) return;
+
+      const percent = floor(((footerVisibleOnScreen / (100 / parallaxPercentage)) / window.innerHeight) * 100, 1);
+      if (percent > parallaxPercentage) return;
+
+      setAnimationOffset(parallaxPercentage - percent);
+    };
+
     handleFooterSizeChange();
     new ResizeObserver(handleFooterSizeChange).observe(footerRef.current);
-  }, []);
+    window.addEventListener('scroll', handleAnimation);
+
+    return () => {
+      window.removeEventListener('scroll', handleAnimation);
+    };
+  }, [isTablet]);
 
   const sitemap = [
     { label: t('routes.home'), value: routes.home },
@@ -41,7 +63,13 @@ export default function Footer() {
   ];
 
   return (
-    <footer ref={footerRef} style={isTablet ? {} : { bottom: bottomOffset }}>
+    <footer
+      ref={footerRef}
+      style={isTablet ? {} : {
+        bottom: bottomOffset,
+        transform: `translateY(${animationOffset}%)`,
+      }}
+    >
       <div className="content-container">
         <div className="identity-wrapper">
           {isLaptop ? (
